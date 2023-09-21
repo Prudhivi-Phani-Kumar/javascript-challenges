7e7f744973ae7e27ea5121541dffe055653204b8
Prudhivi-Phani-Kumar <138499569+Prudhivi-Phani-Kumar@users.noreply.github.com>
2023-09-21 13:39:20 +0530

    Polyfill's 😍 : for Map, filter, reduce, [Array] -- call, bind, apply [Function]  -- split [String]
    Polyfill's 😍 : for Map, filter, reduce, [Array] -- call, bind, apply [Function]  -- split [String]

--- polyfill.js
+++ polyfill.js
@@ -1,8 +1,11 @@
 /**
  * Polyfill 😍
- * for Map, filter, reduce,
- * call, bind, apply
+ * for Map, filter, reduce, [Array]
+ * call, bind, apply [Function]
+ * split [String]
  * */
+
+const ar = [20, 40, 60];
 
 // Map polyfill:
 Array.prototype.mapPolyfill = function (cb) {
@@ -84,4 +87,51 @@ Function.prototype.bindPolyfill = function (context = {}, ...args) {
 }
 userInfo.bind(ob, "Manager")()
 userInfo.bindPolyfill(ob, "Sr. Manager")()
+
+//String Split polyfill
+
+const str = "The quick brown fox jumps over the lazy dog";
+
+//polyfill
+String.prototype.mySplit = function (delimitter) {
+	const sp = (string, delimiter) => {
+		const result = []
+		if (delimiter === "") return Array.from(string)
+		const startSplit = (str, i) => {
+			if (i >= string.length) return
+			const index = str.indexOf(delimiter)
+			if (index >= 0) {
+				result.push(str.substring(0, index))
+				startSplit(str.substring(index + delimiter.length), index + delimiter.length)
+			} else {
+				result.push(str)
+			}
+		}
+		startSplit(string, 0)
+		return result
+	}
+	return sp(this.toString(), delimitter)
+}
+
+// method implementation
+const sp = (string, delimiter) => {
+	const result = []
+	if (delimiter === "") return Array.from(string)
+	const startSplit = (str, i) => {
+		if (i >= string.length) return
+		const index = str.indexOf(delimiter)
+		if (index >= 0) {
+			result.push(str.substring(0, index))
+			startSplit(str.substring(index + delimiter.length), index + delimiter.length)
+		} else {
+			result.push(str)
+		}
+	}
+	startSplit(string, 0)
+	return result
+}
+
+console.log(sp(str, " "));
+console.log(str.mySplit(" "));
+
 
